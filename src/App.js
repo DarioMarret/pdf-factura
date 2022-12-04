43349b0334c82a7584fbddcbda5c2d63073a36f8
DarioMarret <javier_dario_marret@hotmail.com>
2022-12-03 20:18:19 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/alt-text */
 import './App.css';
+import {useState, useEffect} from 'react';
+import { decode } from 'js-base64';
 
 function App() {
+ 
+  const [data, setData] = useState({
+    
+  });
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    // PRUEBAS
+    // const query = link.split('?')[1]
+    // if (query) {
+    //   setData(JSON.parse(decode(query)))
+    // }
+    // PRODUCCION
+    console.log(window.location.search)
+    if (window.location.search.split('?')[1]) {
+      console.log('entro', JSON.parse(decode(window.location.search.split("?")[1])))
+      setData(JSON.parse(decode(window.location.search.split("?")[1])))
+    }
+    setLoader(false)
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {loader ? <div></div>:
+      <div className="factura"
+        style={{
+          marginTop: '10px',
+        }}
+      >
+        <div className="factura_empresa">
+          <div className='factura_empresa_datos'
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <img className='factura_empresa_datos_logo' src={data.logo_empresa} />
+            <div className='factura_empresa_datos_info'>
+              <p className='bold'>{data.infoTributaria.nombreComercial}</p>
+              <p
+                style={{
+                  fontSize: '12px',
+                }}
+              >{data.infoTributaria.dirMatriz}</p>
+              <p
+                style={{
+                  fontSize: '12px',
+                }}
+              >{data.infoFactura.obligadoContabilidad === 'SI' ? 'Obligado a llevar contabilidad' : 'No obligado a llevar contabilidad'}</p>
+            </div>
+          </div>
+          <div className='factura_empresa_infotrib'
+            style={{
+              // backgroundColor: '#292929',
+            }}>
+          
+              <p className='bold'>R.U.C.: {data.infoTributaria.ruc}</p>
+              <p>Factura - {data.infoTributaria.estab}-{data.infoTributaria.ptoEmi}-{data.infoTributaria.secuencial}</p>
+              <div>
+                <p className='bold'>Clave de acceso </p>
+                <p  
+                  style={{
+                    fontSize: '12px',
+                  }}
+                >{data.infoTributaria.claveAcceso}</p>
+              </div>
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <p className='bold'>Ambiente: {data.infoTributaria.ambiente === '1' ? 'Prueba' : 'Produccion'}</p>
+                <p className='bold'>Emisión: Normal</p>
+              </div>
+          </div>
+        </div>
+        <div className="factura_cliente">
+          <div className='factura_cliente_dato'>
+            <p><strong>Fecha:</strong> {data.infoFactura.fechaEmision}</p>
+            <p><strong>Cliente:</strong> {data.infoFactura.razonSocialComprador}</p>
+          </div>
+          <div className='factura_cliente_dato'>
+            <p><strong>Dirección:</strong> {data.infoFactura.direccionComprador}</p>
+            <p><strong>Cedula:</strong> {data.infoFactura.identificacionComprador}</p>
+          </div>
+        </div>
+        <div className="factura_detalle">
+          <table className='factura_detalle_tabla'>
+            <thead className=''
+              // style={{
+              //   backgroundColor: '#292929',
+              // }}
+            >
+              <tr className='factura_detalle_tabla_header'>
+                <th className='width-60 textleft padd-l-6'>Descripción</th>
+                <th className='pad-2'>Cant.</th>
+                <th className='pad-2'>Precio unit.</th>
+                <th className='pad-2'>Desc.</th>
+                <th className='pad-2'>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                data.detalles.detalle.map((detail, index) => {
+                return <tr key={index} className=''>
+                  <td className='pad-2 textleft  padd-l-6 '>{detail.descripcion}</td>
+                  <td className='pad-2 factura_detalle_tabla_fila_elem'>{detail.cantidad}</td>
+                  <td className='pad-2 factura_detalle_tabla_fila_elem'>{detail.precioUnitario}</td>
+                  <td className='pad-2 factura_detalle_tabla_fila_elem'>{detail.descuento}</td>
+                  <td className='pad-2 factura_detalle_tabla_fila_elem'>{detail.precioTotalSinImpuesto}</td>
+                </tr>
+                })
+              }
+            </tbody>
+          </table>
+          <div className='factura_totalescontainer'>
+            <table className='factura_totalescontainer_tabla'>
+              <tr className='factura_totalescontainer_tabla_hearder'>
+                <th>Total sin I.V.A.</th>
+                <th>I.V.A.</th>
+                <th>Total con I.V.A.</th>
+              </tr>
+              <tr className=''>
+                <td className='textcenter'>${data.infoFactura.totalSinImpuestos}</td>
+                <td className='textcenter'>12.00%</td>
+                <td className='textcenter'>${data.infoFactura.importeTotal}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    }
     </div>
   );
 }
